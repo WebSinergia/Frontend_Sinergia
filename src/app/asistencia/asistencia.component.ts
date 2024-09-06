238e03249e380831bf39072291eb208e54e612b5
Caleb <reateguicevallosc@gmail.com>
2024-09-05 22:09:25 -0500

    Add: Spiner for Asistence View

--- src/app/asistencia/asistencia.component.ts
+++ src/app/asistencia/asistencia.component.ts
@@ -11,6 +11,7 @@ export class AsistenciaComponent {
   userId: number = 0;
   userData: any = {};
   asistenciaMarcada = false;
+  loading: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
@@ -26,9 +27,11 @@ export class AsistenciaComponent {
   }
 
   marcarAsistencia(){
+    this.loading = true;
     this.inscripcionesService.checkAsistenceDay1(this.userId).subscribe(
       (data) => {
         console.log('Asistencia marcada con exito');
+        this.loading = false;
         this.asistenciaMarcada = true;
         setTimeout(() => {
           window.location.href = '/home';
@@ -36,19 +39,23 @@ export class AsistenciaComponent {
       },
       (error) => {
         console.error('Error al obtener los datos del usuario:', error);
+        this.loading = false;
       }
     )
   }
 
   getUserData() {
+    this.loading = true;
     if (this.userId) {
       this.inscripcionesService.getUserByID(this.userId).subscribe(
         (data) => {
           this.userData = data;
+          this.loading = false;
           console.log(this.userData);
         },
         (error) => {
           console.error('Error al obtener los datos del usuario:', error);
+          this.loading = false;
         }
       );
     }
